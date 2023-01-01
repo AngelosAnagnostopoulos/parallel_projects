327fd24fdcc136965fd2b66229e423cdee3b6c34
Angelos Anagnostopoulos <angelosanag@protonmail.com>
2023-01-02 01:40:40 +0200

    fixed error on compile script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+mkdir ./bin
 g++ -O2 ./pi_aprox/pi_integral_openmp.c -o ./bin/pi_openmp -lgomp -lm -fopenmp
 g++ -O2 ./euler_aprox/euler_openmp.c -o ./bin/euler -lgomp -lm -fopenmp
 g++ -O2 ./ln_aprox/ln_openmp.c -o ./bin/ln_openmp -lgomp -lm -fopenmp
